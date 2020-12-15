c0b2c6584f182a8cca58ee64a135927f7c1a559d
Nadine-Fadlallah <nadine.fadlallah98@gmail.com>
2020-12-15 23:04:23 +0200

    Exercise 3.16 Solution

--- index.js
+++ index.js
@@ -27,8 +27,15 @@ app.get('/api/persons', (request, response) => {
 
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => response.json(person))
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person)
+                response.json(person)
+            else
+                response.status(404).end()
+        })
+        .catch(error => next(error))
 
 })
 
@@ -37,9 +44,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 
 })
-
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -64,6 +71,18 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => response.json(savedPerson))
 
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
