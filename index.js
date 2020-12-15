92dc1bc86ca19ecca2d09de167183c73194f589c
Nadine-Fadlallah <nadine.fadlallah98@gmail.com>
2020-12-15 22:41:31 +0200

    Exercise 3.15 Solution

--- index.js
+++ index.js
@@ -32,11 +32,12 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
 
-    response.status(204).end()
 })
 
 
